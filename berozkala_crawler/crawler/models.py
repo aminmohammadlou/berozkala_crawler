7d7d880a2d176de75528862b85b95a91a9174ef4
amin <aminmohammadlou99@gmail.com>
2021-08-27 21:44:41 +0430

    create models and make migrations

--- berozkala_crawler/crawler/models.py
+++ berozkala_crawler/crawler/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.fields import CharField, IntegerField, URLField
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+class Category(models.Model):
+    name = CharField(verbose_name='Name', max_length=200)
+    url = URLField()
+
+class Product(models.Model):
+    name = CharField(verbose_name='Name', max_length=200)
+    price = IntegerField(verbose_name='Price', help_text="In Tuman")
+    category = ForeignKey(Category, on_delete=models.CASCADE)
