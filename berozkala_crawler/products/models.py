ddead6770e65604c42269788c4e19caa8f400dee
amin <aminmohammadlou99@gmail.com>
2021-09-03 16:23:40 +0430

    Add meta class and some fields for products.models

--- berozkala_crawler/products/models.py
+++ berozkala_crawler/products/models.py
@@ -2,9 +2,27 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(verbose_name='Name', max_length=200, primary_key=True)
-    url = models.URLField()
+    url = models.URLField(verbose_name='URL', unique=True, blank=False)
+    created_time = models.DateTimeField('Created Time', auto_now_add=True)
+    updated_time = models.DateTimeField('Updated Time', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
 class Product(models.Model):
     name = models.CharField(verbose_name='Name', max_length=200, primary_key=True)
     price = models.IntegerField(verbose_name='Price', help_text="In Tuman")
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_time = models.DateTimeField('Created Time', auto_now_add=True)
+    updated_time = models.DateTimeField('Updated Time', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
