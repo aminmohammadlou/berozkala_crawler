3eb5566d644a89ab1f986a62f0f936a0ea9e5f21
amin <aminmohammadlou99@gmail.com>
2021-09-04 17:28:41 +0430

    Add register function to users.views

--- berozkala_crawler/users/forms.py
+++ berozkala_crawler/users/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import fields
-from django.forms.widgets import PasswordInput
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
--- berozkala_crawler/users/views.py
+++ berozkala_crawler/users/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 from django.views import generic
 from .models import Profile
 
 def homepage(request):
     return render(request=request, template_name='users/home.html')
+
+def register_request(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful')
+            return redirect('users:homepage')
+
+        messages.error(request, 'Unsuccessful registration')
+    form = NewUserForm()
+    return render(request=request, template_name='users/register.html', context={'register_form':form})
 
 class ProfileListView(generic.ListView):
     template_name = 'users/profile_list.html'
