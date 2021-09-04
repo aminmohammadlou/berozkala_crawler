826b6dd9516cf22d9da034c5a9be297e13869b9e
amin <aminmohammadlou99@gmail.com>
2021-09-04 17:16:57 +0430

    Modify register.html and add header.html

--- berozkala_crawler/berozkala_crawler/urls.py
+++ berozkala_crawler/berozkala_crawler/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include('users.urls', namespace='users')),
     path('products/', include('products.urls')),
     path('users/', include('users.urls')),
 ]
