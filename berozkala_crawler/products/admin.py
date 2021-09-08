d4f1c59ad6e293436616368bb30d36d126a785aa
amin <aminmohammadlou99@gmail.com>
2021-09-08 23:04:30 +0430

    Add id to admin list displays

--- berozkala_crawler/products/admin.py
+++ berozkala_crawler/products/admin.py
@@ -4,9 +4,9 @@ from .models import Product, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_time', 'updated_time')
+    list_display = ('id', 'name', 'created_time', 'updated_time')
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     ordering = ('category',)
-    list_display = ('name', 'price', 'category')
+    list_display = ('id', 'name', 'price', 'category')
