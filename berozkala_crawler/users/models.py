31457448beacdd0c267b36fd71c2fe411900b9c9
amin <aminmohammadlou99@gmail.com>
2021-09-12 13:07:08 +0430

    Modify users model and make migrations

--- berozkala_crawler/users/models.py
+++ berozkala_crawler/users/models.py
@@ -5,28 +5,25 @@ from django.core.validators import RegexValidator
 
 
 class UserManager(BaseUserManager):
-    use_in_migrations = True
-    
-    def _create_user(self, phone_number, password, **kwargs):
-
+    def create_user(self, phone_number, password=None):
         if not phone_number:
-            raise ValueError('The given phone number must be set')
-        user = self.model(phone_number=phone_number, **kwargs)
+            raise ValueError(_('Users must have a phone_number'))
+
+        user = self.model(phone_number=phone_number)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
 
-    def create_user(self, phone_number, password=None, **kwargs):
-        kwargs.setdefault('is_superuser', False)
-        return self._create_user(phone_number, password, **kwargs)
+    def create_superuser(self, phone_number, password=None):
+        if not password:
+            raise TypeError(_('Password must be set'))
 
-    def create_superuser(self, phone_number, password, **kwargs):
-        kwargs.setdefault('is_superuser', True)
-
-        if kwargs.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(phone_number, password, **kwargs)
+        user = self.create_user(phone_number, password=password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_verified = True
+        user.save()
+        return user
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
@@ -37,15 +34,24 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     phone_number = models.PositiveBigIntegerField(_('phone number'), unique=True, validators=[phone_number_validator])
     first_name = models.CharField(_('first name'), max_length=100, blank=True, null=False)
     last_name = models.CharField(_('last name'), max_length=100, blank=True, null=False)
+    is_active = models.BooleanField(_('is active'), default=True)
+    is_verified = models.BooleanField(_('is verified'), default=True)
+    is_staff = models.BooleanField(_('is staff'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
 
     USERNAME_FIELD = 'phone_number'
+    
 
     objects = UserManager()
+
+    def __str__(self):
+        return self.phone_number
+        
 
     class Meta:
         db_table = 'user'
         verbose_name = _('User')
         verbose_name_plural = _('Users')
 
-    def __str__(self):
-        return f'phone number: {self.phone_number} - first name: {self.first_name} - last name: {self.last_name}'
+    
